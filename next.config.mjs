d932c22b8108d0771daf0ebb0a43bfaaca239ae9
Meowell <Meowell@users.noreply.github.com>
2026-05-06 22:00:56 +0800

    chore: suppress eslint img warnings during build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import path from "node:path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
   webpack(config) {
     config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
     return config;
